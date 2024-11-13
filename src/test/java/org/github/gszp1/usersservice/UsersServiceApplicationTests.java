3794c16452c4e78e93a51a18513f7dd7bde6854a
gszp1 <grzybekmateusz@poczta.fm>
2024-11-13 16:07:14 +0100

    Initial commit.

---
+++ src/test/java/org/github/gszp1/usersservice/UsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.gszp1.usersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
