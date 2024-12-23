064b787fce57457be958d4f25c1488d7aea32016
gszp1 <grzybekmateusz@poczta.fm>
2024-12-23 21:11:30 +0100

    Adds frontend configuration.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+import { resolve, dirname } from 'path'
+
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
+  server: { port: 9091 },
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  }
 })
