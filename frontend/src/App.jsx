ca5231488f87dac8b18621b027db7ab799164e72
gszp1 <grzybekmateusz@poczta.fm>
2024-12-23 22:29:18 +0100

    Refactors Navbar.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router";
 import HomePage from "@/components/HomePage/HomePage";
-
+import NavigationBar from "@/components/NavigationBar/NavigationBar.module.css"
 
 function App() {
   return (
     <>
+      <NavigationBar
+      />
       <Routes>
         <Route path='/' element={<HomePage/>}></Route>
         
---
+++ frontend/src/components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,10 @@
+import styles from "@/components/Navbar/NavigationBar.module.css"
+
+const NavigationBar = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default NavigationBar;
