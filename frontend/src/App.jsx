f5c71388e45bc89c0020c451f8965e51d3e903f5
gszp1 <grzybekmateusz@poczta.fm>
2024-12-23 22:22:49 +0100

    Adds routing to app.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,13 @@
+import { Routes, Route } from "react-router";
+import HomePage from "@/components/HomePage/HomePage";
+
+
 function App() {
   return (
     <>
+      <Routes>
+        <Route path='/' element={<HomePage/>}></Route>
+      </Routes>
     </>
   )
 }
