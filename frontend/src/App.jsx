e73b6d29c5bf775fa74ef692069ec2e4fae656dd
gszp1 <grzybekmateusz@poczta.fm>
2024-12-26 16:14:00 +0100

    Adds Routes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import NavigationBar from "@/components/NavigationBar/NavigationBar.jsx"
+import HomePage from "@/components/HomePage/HomePage.jsx"
+import {Routes, Route} from "react-router";
 
 function App() {
   return (
     <>
       <NavigationBar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+      </Routes>
     </>
   )
 }
