a33d357f4a21b6dbbd562b142450af5430637159
gszp1 <grzybekmateusz@poczta.fm>
2024-12-27 16:16:36 +0100

    Adds ShoppingCartPage component.

---
+++ frontend/src/components/ShoppingCartPage/ShoppingCartPage.jsx
@@ -0,0 +1,11 @@
+import styles from '@/components/ShoppingCartPage/ShoppingCartPage.module.css'
+
+const ShoppingCartPage = () => {
+    return (
+        <div className={styles.page}>
+
+        </div>
+    );
+}
+
+export default ShoppingCartPage;
