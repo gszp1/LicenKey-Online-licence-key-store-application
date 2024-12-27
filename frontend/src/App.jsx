51a78e44bdc5170c60d037fb08dbfd2f91357dac
gszp1 <grzybekmateusz@poczta.fm>
2024-12-27 01:21:33 +0100

    Adds regsiter page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import NavigationBar from "@/components/NavigationBar/NavigationBar.jsx"
 import HomePage from "@/components/HomePage/HomePage.jsx"
-import LoginPage from "@/components/LoginPage/LoginPage";
+import LoginPage from "@/components/LoginPage/LoginPage.jsx";
+import RegisterPage from "@/components/RegisterPage/RegisterPage.jsx";
+
 import {Routes, Route} from "react-router";
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/register" element={<RegisterPage/>}/>
       </Routes>
     </>
   )
---
+++ frontend/src/components/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,11 @@
+import styles from "@/components/RegisterPage/RegisterPage.module.css"
+
+const RegisterPage = () => {
+    return (
+        <div className={styles.page}>
+            Register
+        </div>
+    );
+}
+
+export default RegisterPage;
