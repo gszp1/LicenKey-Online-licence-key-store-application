41cad45e13b19ce3a3b6a4122082dc67b975e936
gszp1 <grzybekmateusz@poczta.fm>
2024-12-27 18:42:23 +0100

    Adds requirements.

--- frontend/src/components/RegisterPage/RegisterPage.jsx
+++ frontend/src/components/RegisterPage/RegisterPage.jsx
@@ -1,4 +1,5 @@
 import styles from "@/components/RegisterPage/RegisterPage.module.css"
+import { Link } from "react-router";
 
 const RegisterPage = () => {
     return (
@@ -44,16 +45,19 @@ const RightSection = () => {
     return (
         <div className={styles.right_section}>
             <div className={styles.secondary_box}>
-                <label className={styles.input_label}> Name </label>
+                <label className={styles.input_label}> Name* </label>
                 <input className={styles.input_field}></input>
                 <p className={styles.error_prompt}>error placeholder</p>
-                <label className={styles.input_label}> Second Name </label>
+                <label className={styles.input_label}> Second Name* </label>
                 <input className={styles.input_field}></input>
                 <p className={styles.error_prompt}>error placeholder</p>
             </div>
             <button className={styles.register_button}>
                 Sign Up
             </button>
+            <Link to="/login" style={{marginTop: '5px'}}>
+                <p>Already have an account?</p>
+            </Link>
         </div>
     );
 }
@@ -61,7 +65,28 @@ const RightSection = () => {
 const Requirements = () => {
     return (
         <div className={styles.requirements}>
-            requirements
+            <ol>
+                <li>Password must include:
+                    <ul>
+                        <li>1 Capital character</li>
+                        <li>1 Small character</li>
+                        <li>1 Special character</li>
+                        <li>1 Numeric character</li>
+                    </ul>
+                </li>
+                <li>
+                    Fields marked with * are optional - you can fill them later in account page.
+                </li>
+                <li>
+                    You can create only one account per email.
+                </li>
+                <li>
+                    Your username must be unique.
+                </li>
+                <li>
+                    Values in given field and confirm field must be equal.
+                </li>
+            </ol>
         </div>
     );
 }
