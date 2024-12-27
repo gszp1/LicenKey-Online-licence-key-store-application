176bb995c2a8bf826d21ddcd123fe3ba280c4eea
gszp1 <grzybekmateusz@poczta.fm>
2024-12-27 16:52:15 +0100

    Rewrites RegisterPage structure.

--- frontend/src/components/RegisterPage/RegisterPage.jsx
+++ frontend/src/components/RegisterPage/RegisterPage.jsx
@@ -3,12 +3,41 @@ import styles from "@/components/RegisterPage/RegisterPage.module.css"
 const RegisterPage = () => {
     return (
         <div className={styles.page}>
-            <div className={styles.credentials}>
-                credentials
-            </div>
-            <div className={styles.requirements}>
-                credentials requirements
-            </div>
+            <Credentials/>
+            <Requirements/>
+        </div>
+    );
+}
+
+const Credentials = () => {
+    return (
+        <div className={styles.credentials}>
+            <MainBox/>
+            <SecondaryBox/>
+        </div>
+    );
+}
+
+const MainBox = () => {
+    return (
+        <div className={styles.main_box}>
+
+        </div>
+    );
+}
+
+const SecondaryBox = () => {
+    return (
+        <div className={styles.secondary_box}>
+
+        </div>
+    );
+}
+
+const Requirements = () => {
+    return (
+        <div className={styles.requirements}>
+            
         </div>
     );
 }
