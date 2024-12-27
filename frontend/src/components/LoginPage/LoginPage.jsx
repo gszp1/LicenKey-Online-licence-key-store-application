8c7e4ab100e032cea4f70a800af19f9a06d94c81
gszp1 <grzybekmateusz@poczta.fm>
2024-12-27 12:26:55 +0100

    Adds login page structure.

--- frontend/src/components/LoginPage/LoginPage.jsx
+++ frontend/src/components/LoginPage/LoginPage.jsx
@@ -3,7 +3,21 @@ import styles from "@/components/LoginPage/LoginPage.module.css"
 const LoginPage = () => {
     return (
         <div className={styles.page}>
-            login
+            <div className={styles.login_box}>
+                Login
+                <label>email</label>
+                <input/>
+                <p>error placeholder</p>
+                <label>password</label>
+                <input/>
+                <p>error placeholder</p>
+                <button>Login</button>
+            </div>
+            <div className={styles.register_box}>
+                Register
+                <p>Don't Have an Account yet?</p>
+                <button>Sign Up</button>
+            </div>
         </div>
     );
 }
