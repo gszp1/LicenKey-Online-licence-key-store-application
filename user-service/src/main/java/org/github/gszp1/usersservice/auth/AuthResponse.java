58c01fd4ce6886b91da4fb07e03824fe7eebd9db
gszp1 <grzybekmateusz@poczta.fm>
2024-12-28 20:44:07 +0100

    Adds AuthResponse object.

---
+++ user-service/src/main/java/org/github/gszp1/usersservice/auth/AuthResponse.java
@@ -0,0 +1,14 @@
+package org.github.gszp1.usersservice.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AuthResponse {
+
+    public String content;
+}
