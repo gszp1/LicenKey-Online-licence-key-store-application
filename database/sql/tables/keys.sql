74f18621ce982550b4a32e8729c75454682002b3
gszp1 <grzybekmateusz@poczta.fm>
2025-01-16 22:38:39 +0100

    Adds keys.sql table.

---
+++ database/sql/tables/keys.sql
@@ -0,0 +1,9 @@
+CREATE TABLE keys (
+    creation_date TIMESTAMPTZ NOT NULL DEFAULT now(),
+    update_date TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TRIGGER refresh_update_date_trg
+BEFORE UPDATE ON licences
+FOR EACH ROW
+EXECUTE FUNCTION refresh_update_date();
