6943635e19262019f966a6e3e3fa81879994cc82
gszp1 <grzybekmateusz@poczta.fm>
2025-01-16 22:59:39 +0100

    Adds licence_types.sql.

---
+++ database/sql/tables/licence_types.sql
@@ -0,0 +1,11 @@
+CREATE TABLE licence_types (
+    type_id BIGSERIAL PRIMARY KEY
+    'name' licence_type NOT NULL DEFAULT 'lifetime'
+    creation_date TIMESTAMPTZ NOT NULL DEFAULT now(),
+    update_date TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TRIGGER refresh_update_date_trg
+BEFORE UPDATE ON licences
+FOR EACH ROW
+EXECUTE FUNCTION refresh_update_date();
