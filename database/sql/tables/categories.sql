af062b24d3e13cc483c680a32cb1d48c053add14
gszp1 <grzybekmateusz@poczta.fm>
2025-01-16 23:48:33 +0100

    Adds categories.sql.

---
+++ database/sql/tables/categories.sql
@@ -0,0 +1,11 @@
+CREATE TABLE categories (
+    category_id BIGSERIAL PRIMARY KEY,
+    'name' VARCHAR(100) NOT NULL UNIQUE,
+    creation_date TIMESTAMPTZ NOT NULL DEFAULT now(),
+    update_date TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TRIGGER refresh_update_date_trg
+BEFORE UPDATE ON licences
+FOR EACH ROW
+EXECUTE FUNCTION refresh_update_date();
