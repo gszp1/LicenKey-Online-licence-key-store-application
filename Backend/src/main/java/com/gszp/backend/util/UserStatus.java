7aea4aff2bb4cceb2d92cdafadeb35dda1c054b8
gszp1 <grzybekmateusz@poczta.fm>
2025-01-17 22:49:35 +0100

    Adds UserStatus enum.

---
+++ Backend/src/main/java/com/gszp/backend/util/UserStatus.java
@@ -0,0 +1,25 @@
+package com.gszp.backend.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@AllArgsConstructor
+@Getter
+public enum UserStatus {
+    ACTIVE("active"),
+    DEACTIVATED("deactivated"),
+    BANNED("banned");
+
+    private final String databaseValue;
+
+    private static final Map<String, UserStatus> DB_VALUES =
+        Stream.of(values()).collect(Collectors.toMap(UserStatus::getDatabaseValue, UserStatus -> UserStatus));
+
+    public static UserStatus getByDatabaseValue(String databaseValue) {
+        return DB_VALUES.get(databaseValue);
+    }
+}
