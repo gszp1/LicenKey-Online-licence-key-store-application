fbdd23ac89f8793ce1ccf9768d05cdafaaf01371
gszp1 <grzybekmateusz@poczta.fm>
2025-01-17 22:54:53 +0100

    Adds UserRole enum.

---
+++ Backend/src/main/java/com/gszp/backend/util/UserRole.java
@@ -0,0 +1,24 @@
+package com.gszp.backend.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@AllArgsConstructor
+@Getter
+public enum UserRole {
+    USER("user"),
+    ADMIN("admin");
+
+    private final String databaseValue;
+
+    public static final Map<String, UserRole> DB_VALUES =
+            Stream.of(values()).collect(Collectors.toMap(UserRole::getDatabaseValue, UserRole -> UserRole));
+
+    public UserRole getFromDatabaseValue(String databaseValue) {
+        return DB_VALUES.get(databaseValue);
+    }
+}
