810158987844e82594980b916c277ac67f1cb4c3
gszp1 <grzybekmateusz@poczta.fm>
2025-01-17 23:03:26 +0100

    Adds UserStatusConverter class.

---
+++ Backend/src/main/java/com/gszp/backend/util/UserStatusConverter.java
@@ -0,0 +1,21 @@
+package com.gszp.backend.util;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class UserStatusConverter implements AttributeConverter<UserStatus, String> {
+
+    @Override
+    public String convertToDatabaseColumn(UserStatus userStatus) {
+        if (userStatus == null) {
+            return null;
+        }
+        return userStatus.getDatabaseValue();
+    }
+
+    @Override
+    public UserStatus convertToEntityAttribute(String databaseValue) {
+        return UserStatus.getByDatabaseValue(databaseValue);
+    }
+}
