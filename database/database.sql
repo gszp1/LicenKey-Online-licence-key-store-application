220cff7e7dd41b6bd417d36d505b576f5c6cb6a2
gszp1 <grzybekmateusz@poczta.fm>
2025-01-17 19:10:32 +0100

    Fixes keys table. | Adds database diagram.

--- database/database.sql
+++ database/database.sql
@@ -127,13 +127,13 @@ EXECUTE FUNCTION refresh_update_date();
 
 CREATE TABLE keys (
     key_id BIGSERIAL PRIMARY KEY, 
-    expired BOOLEAN NOT NULL DEFAULT VALUE FALSE,
-    user_id BIGINT,
+    expired BOOLEAN NOT NULL DEFAULT FALSE,
     licence_id BIGINT,
+    user_id BIGINT,
     creation_date TIMESTAMPTZ NOT NULL DEFAULT now(),
     update_date TIMESTAMPTZ NOT NULL DEFAULT now(),
-    FOREIGN KEY (user_id) BIGINT REFERENCES users(user_id) ON DELETE RESTRICT,
-    FOREIGN KEY (licence_id) BIGINT REFERENCES licences(licence_id) ON DELETE RESTRICT
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE RESTRICT,
+    FOREIGN KEY (licence_id) REFERENCES licences(licence_id) ON DELETE RESTRICT
 
 );
 
