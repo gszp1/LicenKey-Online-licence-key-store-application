ae631a0c66174e44d6ab7e5b1862ead49bb8a5bb
gszp1 <grzybekmateusz@poczta.fm>
2025-01-17 01:12:04 +0100

    Adds relationships to services.

--- database/sql/tables/services.sql
+++ database/sql/tables/services.sql
@@ -1,6 +1,7 @@
 CREATE TABLE services (
     service_id BIGSERIAL PRIMARY KEY,
     api_url VARCHAR(2083) NOT NULL,  /* Currently max length of url*/
+    FOREIGN KEY (publisher_id) BIGINT REFERENCES publishers(publisher_id) ON DELETE CASCADE,
     creation_date TIMESTAMPTZ NOT NULL DEFAULT now(),
     update_date TIMESTAMPTZ NOT NULL DEFAULT now()
 );
