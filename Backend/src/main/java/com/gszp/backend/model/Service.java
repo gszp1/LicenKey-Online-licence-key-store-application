c34eb76dcb28df3bf17188c481853afbe0683352
gszp1 <grzybekmateusz@poczta.fm>
2025-01-17 22:03:52 +0100

    Adds Service entity class.

---
+++ Backend/src/main/java/com/gszp/backend/model/Service.java
@@ -0,0 +1,22 @@
+package com.gszp.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "services")
+public class Service {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "service_id")
+    private Long serviceId;
+
+    @Column(length = 2083, nullable = false)
+    private String apiUrl;
+}
