06b852867861ff7ae188687399a5ea94b8cd56f3
gszp1 <grzybekmateusz@poczta.fm>
2025-01-17 17:09:12 +0100

    Adds platforms table to database.sh.

--- database/database.sql
+++ database/database.sql
@@ -9,6 +9,19 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE TABLE platforms (
+    platform_id BIGSERIAL PRIMARY KEY,
+    'name' VARCHAR(128) UNIQUE NOT NULL,
+    home_page VARCHAR(2083),
+    creation_date TIMESTAMPTZ NOT NULL DEFAULT now(),
+    update_date TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TRIGGER refresh_update_date_trg
+BEFORE UPDATE ON licences
+FOR EACH ROW
+EXECUTE FUNCTION refresh_update_date();
+
 CREATE TABLE licence_types (
     type_id BIGSERIAL PRIMARY KEY,
     duration_days INTEGER,
