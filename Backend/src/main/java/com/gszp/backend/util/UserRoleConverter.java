be772d31d1cc34d103bdc76b4a9f5a3e1dd43c58
gszp1 <grzybekmateusz@poczta.fm>
2025-01-17 22:59:34 +0100

    Renames RoleConverter to UserRoleConverter.

--- Backend/src/main/java/com/gszp/backend/util/UserRoleConverter.java
+++ Backend/src/main/java/com/gszp/backend/util/UserRoleConverter.java
@@ -4,7 +4,7 @@ import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
 @Converter(autoApply = true)
-public class RoleConverter implements AttributeConverter<UserRole, String> {
+public class UserRoleConverter implements AttributeConverter<UserRole, String> {
 
     @Override
     public String convertToDatabaseColumn(UserRole userRole) {
