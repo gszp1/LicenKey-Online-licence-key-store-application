be514e82ac53f5bb66e376b349245f08959f66d4
gszp1 <grzybekmateusz@poczta.fm>
2025-01-18 22:12:42 +0100

    Adds orders table.

---
+++ database/sql/tables/orders.sql
@@ -0,0 +1,17 @@
+CREATE TABLE orders (
+    FK_user_id BIGINT,
+    FK_licence_id BIGINT,
+    unit_price DECIMAL(10,2) NOT NULL CHECK(unit_price >= 0),
+    quantity INT NOT NULL CHECK(quantity > 0),
+    order_id UUID NOT NULL,
+    creation_date TIMESTAMPTZ NOT NULL DEFAULT now(),
+    update_date TIMESTAMPTZ NOT NULL DEFAULT now()
+    PRIMARY KEY (FK_user_id, FK_licence_id),
+    FOREIGN KEY (FK_user_id) REFERENCES users(user_id) ON DELETE RESTRICT,
+    FOREIGN KEY (FK_licence_id) REFERENCES licences(licence_id) ON DELETE RESTRICT
+);
+
+CREATE TRIGGER refresh_update_date_trg
+BEFORE UPDATE ON orders
+FOR EACH ROW
+EXECUTE FUNCTION refresh_update_date();
