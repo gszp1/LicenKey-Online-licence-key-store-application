558b98d6ce685fbd7aaa251291124671ffed2f19
gszp1 <grzybekmateusz@poczta.fm>
2025-01-19 00:16:33 +0100

    Adds Order entity class.

---
+++ Backend/src/main/java/com/gszp/backend/model/Order.java
@@ -0,0 +1,40 @@
+package com.gszp.backend.model;
+
+import com.gszp.backend.model.keys.OrdersKey;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "orders")
+public class Order {
+
+    @EmbeddedId
+    private OrdersKey key;
+
+    @ManyToOne()
+    @MapsId("userId")
+    @JoinColumn(name = "FK_user_id")
+    private User user;
+
+    @ManyToOne()
+    @MapsId("licenceId")
+    @JoinColumn(name = "FK_licence_id")
+    private Licence licence;
+
+    @Column(name = "unit_price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal unitPrice;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private UUID orderId;
+}
