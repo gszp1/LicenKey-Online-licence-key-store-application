dd216403268e4ab3639d4e684485fca01d01c527
gszp1 <grzybekmateusz@poczta.fm>
2025-01-18 21:51:43 +0100

    Adds shopping_carts table.

---
+++ database/sql/tables/shopping_carts.sql
@@ -0,0 +1,16 @@
+CREATE TABLE shopping_carts (
+    FK_user_id BIGINT,
+    FK_licence_id BIGINT,
+    quantity INT NOT NULL CHECK (quantity > 0),
+    unit_price DECIMAL(10, 2) NOT NULL CHECK (unit_price >= 0),
+    creation_date TIMESTAMPTZ NOT NULL DEFAULT now(),
+    update_date TIMESTAMPTZ NOT NULL DEFAULT now(),
+    PRIMARY KEY (FK_user_id, FK_licence_id),
+    FOREIGN KEY (FK_user_id) REFERENCES users(user_id) ON DELETE RESTRICT,
+    FOREIGN KEY (FK_licence_id) REFERENCES licences(licence_id) ON DELETE RESTRICT
+);
+
+CREATE TRIGGER refresh_update_date_trg
+BEFORE UPDATE ON shopping_carts
+FOR EACH ROW
+EXECUTE FUNCTION refresh_update_date();
