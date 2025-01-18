85451148c9971c7c2823e8207d42a5ae9a0ee500
gszp1 <grzybekmateusz@poczta.fm>
2025-01-18 02:26:18 +0100

    Adds AuthController, AuthService, AuthResponse.

---
+++ Backend/src/main/java/com/gszp/backend/auth/AuthResponse.java
@@ -0,0 +1,10 @@
+package com.gszp.backend.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+public class AuthResponse {
+    private String contents;
+}
