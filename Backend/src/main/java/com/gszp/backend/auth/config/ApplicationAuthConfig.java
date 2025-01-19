f61cefa634792c040e1b1d58bf7324555a2199e4
gszp1 <grzybekmateusz@poczta.fm>
2025-01-19 21:31:55 +0100

    Adds ApplicationAuthConfig config.

---
+++ Backend/src/main/java/com/gszp/backend/auth/config/ApplicationAuthConfig.java
@@ -0,0 +1,50 @@
+package com.gszp.backend.auth.config;
+
+import com.gszp.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static java.lang.String.format;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationAuthConfig {
+
+    private UserRepository userRepository;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(
+                    () ->  new UsernameNotFoundException(format("Username: %s does not exist", username)
+                )
+        );
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
--- Backend/src/main/java/com/gszp/backend/repository/UserRepository.java
+++ Backend/src/main/java/com/gszp/backend/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.gszp.backend.repository;
 
 import com.gszp.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<UserDetails> findByEmail(String email);
 }
