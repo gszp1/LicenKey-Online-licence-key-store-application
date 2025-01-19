ff6e83a19bb836aa9292eeaa45e26a3a82418733
gszp1 <grzybekmateusz@poczta.fm>
2025-01-19 22:40:31 +0100

    Adds JwtAuthFilter.

--- Backend/src/main/java/com/gszp/backend/auth/config/JwtService.java
+++ Backend/src/main/java/com/gszp/backend/auth/config/JwtService.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Base64;
@@ -14,6 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+@Service
 public class JwtService {
 
     @Value("${secret_key}")
