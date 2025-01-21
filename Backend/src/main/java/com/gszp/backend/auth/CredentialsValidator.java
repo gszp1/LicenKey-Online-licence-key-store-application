d737b95753aceefedbb5d0bbb6fcf55b716a05ed
gszp1 <grzybekmateusz@poczta.fm>
2025-01-21 21:40:35 +0100

    Fixes registration 403 error.

---
+++ Backend/src/main/java/com/gszp/backend/auth/CredentialsValidator.java
@@ -0,0 +1,22 @@
+package com.gszp.backend.auth;
+
+import java.util.regex.Pattern;
+
+public class EmailValidator {
+
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+
+    private static final String PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).+$";
+
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
+    private static  final Pattern PASSWORD_PATTERN = Pattern.compile(PASSWORD_REGEX);
+
+    public static boolean validateEmail(String email) {
+        return EMAIL_PATTERN.matcher(email).matches();
+    }
+
+    public static boolean validatePassword(String password) {
+        return PASSWORD_PATTERN.matcher(password).matches();
+    }
+}
