360c8523a8bda07f35ce776fb372c63f6823ab62
gszp1 <grzybekmateusz@poczta.fm>
2025-01-22 22:00:37 +0100

    Adds UserDataUpdateResponse.

---
+++ Backend/src/main/java/com/gszp/backend/dto/response/UserDataUpdateResponse.java
@@ -0,0 +1,13 @@
+package com.gszp.backend.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserDataUpdateResponse {
+
+    private String contents;
+}
