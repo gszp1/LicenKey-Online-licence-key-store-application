b4390a0197ca556d790baaacd76772cda260c479
gszp1 <grzybekmateusz@poczta.fm>
2025-01-23 19:32:18 +0100

    Adds ChangePasswordSection.

---
+++ frontend/src/components/AccountPage/Sections/ChangePassword/ChangePasswordSection.jsx
@@ -0,0 +1,11 @@
+import styles from "@/components/AccountPage/Sections/ChangePassword/ChangePasswordSection.module.css"
+
+const InformationSection = () => {
+    return (
+        <div className={styles.section}>
+            InformationSection-placeholder
+        </div>
+    )
+}
+
+export default InformationSection;
