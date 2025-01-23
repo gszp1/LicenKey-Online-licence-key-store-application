6374fd102d8987c9448e597fd953f434a92782f7
gszp1 <grzybekmateusz@poczta.fm>
2025-01-23 23:16:17 +0100

    Adds fetching user data.

--- frontend/src/components/AccountPage/AccountPage.jsx
+++ frontend/src/components/AccountPage/AccountPage.jsx
@@ -4,10 +4,11 @@ import ModifyInformationSection from "@/components/AccountPage/Sections/ModifyIn
 import OrdersSection from "@/components/AccountPage/Sections/Orders/OrdersSection.jsx"
 import KeysSection from "@/components/AccountPage/Sections/Keys/KeysSection.jsx"
 import ChangePasswordSection from "@/components/AccountPage/Sections/ChangePassword/ChangePasswordSection.jsx"
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavigationMenu from "./NavigationMenu/NavigationMenu"
 import axios from "axios"
+import { jwtDecode } from "jwt-decode"
+import { useNavigate } from "react-router"
 
 const AccountPage = () => {
     const [section, setSection] = useState("information")
@@ -18,9 +19,46 @@ const AccountPage = () => {
         'lastName': '',
         'creationDate': ''
     });
+    const navigate = useNavigate();
     
+    useEffect(() => {
+        const getUserData = async () => {
+            let decodedToken = '';
+            let email = '';
+            try {
+                decodedToken = decodeToken();
+                email = decodedToken.sub;
+            } catch (error) {
+                console.log("TODO" + error);
+            }
+            let url = `${window._env_.BACKEND_API_URL}${'/api/users'}`;
+            try {
+                let response = await axios.post(
+                    url,
+                    {'email': email},
+                    {headers: {
+                        'Authorization': `Bearer ${localStorage.getItem("AuthToken")}`,
+                        "Content-Type": 'application/json'
+                    }}
+                );
+                let data = {
+                    'email': response.data.email,
+                    'username': response.data.username,
+                    'firstName': response.data.firstName,
+                    'lastName': response.data.lastName,
+                    'creationDate': response.data.creationDate
+                };
+                console.log(data);
+                setUserData(data);
+            } catch (error) {
+                console.log("TODO" + error);
+            }
+        }
+        getUserData();
+    }, []);
+
     const decodeToken = () => {
-        let decodedToken = jwt
+        return jwtDecode(localStorage.getItem("AuthToken"));
     }
 
     const renderSection = () => {
@@ -44,8 +82,11 @@ const AccountPage = () => {
         setSection(e.currentTarget.id);
     }
 
-    const logout = (e) => {
-        console.log("TODO" + e);
+    const logout = () => {
+        localStorage.removeItem("AuthToken");
+        setTimeout(() => {
+            navigate("/");
+        }, 400);
     }
 
     return (
