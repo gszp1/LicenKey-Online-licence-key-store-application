e7ee6b2c91949158f570ef6dea7c9e9d2b5379ef
gszp1 <grzybekmateusz@poczta.fm>
2025-01-23 19:37:04 +0100

    Adds Orders section.

---
+++ frontend/src/components/AccountPage/Sections/Orders/OrdersSection.jsx
@@ -0,0 +1,11 @@
+import styles from "@/components/AccountPage/Sections/Orders/OrdersSection.module.css"
+
+const OrdersSection = () => {
+    return (
+        <div className={styles.section}>
+
+        </div>
+    )
+}
+
+export default OrdersSection;
