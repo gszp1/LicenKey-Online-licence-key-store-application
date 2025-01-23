a4a381095bf8a337cfca309cb9df89b2a64b0174
gszp1 <grzybekmateusz@poczta.fm>
2025-01-23 12:54:16 +0100

    Removes user-service.

--- user-service/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t users_service .
-
-docker run --name users_service -d -p 9091:9091 users_service
--- user-service/src/main/java/org/github/gszp1/usersservice/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.github.gszp1.usersservice.repository;
-
-import org.github.gszp1.usersservice.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
