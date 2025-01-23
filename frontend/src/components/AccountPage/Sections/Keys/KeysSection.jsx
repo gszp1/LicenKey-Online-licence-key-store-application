23c8b9cb9c412af8841cbf894aedc6e0562f9740
gszp1 <grzybekmateusz@poczta.fm>
2025-01-23 19:39:19 +0100

    Adds Keys section.

---
+++ frontend/src/components/AccountPage/Sections/Keys/KeysSection.jsx
@@ -0,0 +1,11 @@
+import styles from "@/components/AccountPage/Sections/Keys/KeysSection.module.css"
+
+const OrdersSection = () => {
+    return (
+        <div className={styles.section}>
+
+        </div>
+    )
+}
+
+export default OrdersSection;
