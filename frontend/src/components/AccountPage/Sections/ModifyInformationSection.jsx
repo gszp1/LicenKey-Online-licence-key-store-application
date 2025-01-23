c500df9519153595b5d06d53915ed58e27acd8d1
gszp1 <grzybekmateusz@poczta.fm>
2025-01-23 19:29:31 +0100

    Adds ModifyInformationSection.

---
+++ frontend/src/components/AccountPage/Sections/ModifyInformationSection.jsx
@@ -0,0 +1,11 @@
+import styles from "@/components/AccountPage/Sections/ModifyInformationSection.module.css"
+
+const ModifyInformationSection = () => {
+    return (
+        <div className={styles.section}>
+
+        </div>
+    )
+}
+
+export default ModifyInformationSection;
