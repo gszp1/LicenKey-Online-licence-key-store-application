5f3d3d31ecff8ea2c4cf0f7cf565c07281b3b8a6
gszp1 <grzybekmateusz@poczta.fm>
2025-01-23 18:17:27 +0100

    Adds sending login request.

--- frontend/src/components/LoginPage/LoginPage.jsx
+++ frontend/src/components/LoginPage/LoginPage.jsx
@@ -2,14 +2,16 @@ import styles from "@/components/LoginPage/LoginPage.module.css"
 import { useState } from 'react';
 import { Link } from "react-router";
 import BenefitsList from "@/components/BenefitsList/BenefitsList.jsx";
+import axios from "axios";
+import { useNavigate } from "react-router";
 
 const LoginPage = () => {
     const [errorPrompt, setErrorPrompt] = useState("");
-
     const [credentials, setCredentials] = useState({
         email: "",
         password: ""
     });
+    const navigate = useNavigate();
 
     const updateCredential = (e, credentialName) => {
         let newCredentials = {...credentials,
@@ -33,9 +35,32 @@ const LoginPage = () => {
         return true;
     }
 
-    const login = () => {
-        validateCredentials();
+    const login = async () => {
+        if (validateCredentials() == false) {
+            return;
+        }
+        if (localStorage.getItem("AuthToken")) {
+            setErrorPrompt("You are already logged in.");
+            return;
+        }
         var url = `${window._env_.BACKEND_API_URL}${"/api/auth/login"}`;
+        var loginCredentials = {...credentials}
+        console.log(loginCredentials);
+        try {
+            let response = await axios.post(
+                url,
+                loginCredentials,
+                {headers: {'Content-Type': 'application/json'}}
+            )
+            localStorage.setItem('AuthToken', response.data.contents);
+            navigate("/");
+        } catch (error) {
+            if (error.response && error.response.status === 403) {
+                setErrorPrompt("Account is locked.");
+            } else {
+                setErrorPrompt("Provided credentials are invalid.");
+            }
+        }
     }
 
     return (
