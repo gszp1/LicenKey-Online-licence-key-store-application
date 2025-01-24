8beb71c99c858355d7790798bee2ad2cb3335b6a
gszp1 <grzybekmateusz@poczta.fm>
2025-01-24 19:09:26 +0100

    Changes.

--- frontend/src/components/AccountPage/Sections/ModifyInformation/ModifyInformationSection.jsx
+++ frontend/src/components/AccountPage/Sections/ModifyInformation/ModifyInformationSection.jsx
@@ -1,11 +1,32 @@
 import styles from "@/components/AccountPage/Sections/ModifyInformation/ModifyInformationSection.module.css"
 
-const ModifyInformationSection = () => {
+const InputSection = ({userData, propertyName, header}) => {
+    return (
+        <>
+            <label> {header} </label>
+            <input></input>        
+        </>
+    );
+}
+
+
+const ModifyInformationSection = ({userData, errorHandler}) => {
     return (
         <div className={styles.section}>
-            <h1> Modify Your Account Information </h1>
+            <h1 className={styles.section_header}> Modify Your Account Information </h1>
+            <div className={styles.section_content}>
+                <label> Email </label>
+                <input></input>
+                <label> Username </label>
+                <input></input>
+                <label> First Name </label>
+                <input></input>
+                <label> Last Name </label>
+                <input></input>
+            </div>
         </div>
-    )
+    );
 }
+
 
 export default ModifyInformationSection;
