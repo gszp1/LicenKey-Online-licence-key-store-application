a4ad5b295a22948122782cd528835fc3e2c3c680
gszp1 <grzybekmateusz@poczta.fm>
2025-01-26 00:12:03 +0100

    Adds generating logs in LicenceService.

--- Backend/src/main/java/com/gszp/backend/service/LicenceService.java
+++ Backend/src/main/java/com/gszp/backend/service/LicenceService.java
@@ -4,6 +4,8 @@ import com.gszp.backend.dto.response.LicenceDescriptionResponse;
 import com.gszp.backend.dto.response.LicenceDto;
 import com.gszp.backend.dto.response.LicencesResponse;
 import com.gszp.backend.exception.ResourceNotFoundException;
+import com.gszp.backend.logs.LogGenerator;
+import com.gszp.backend.logs.LogTemplate;
 import com.gszp.backend.model.Licence;
 import com.gszp.backend.repository.LicenceRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,12 +24,15 @@ public class LicenceService {
     ) throws ResourceNotFoundException {
         Optional<Licence> licence = licenceRepository.findById(id);
         if (licence.isEmpty()) {
+            LogGenerator.generateInfoLog(LogTemplate.REQUEST_FAIL, "Licence with given ID does not exist.");
             throw new ResourceNotFoundException("Licence with given id does not exist.");
         }
+        LogGenerator.generateInfoLog(LogTemplate.REQUEST_SUCCESS, "Successfully retrieved licence description.");
         return new LicenceDescriptionResponse(licence.get().getDescription());
     }
 
     public LicencesResponse getLicences(String keyword) {
+        LogGenerator.generateInfoLog(LogTemplate.REQUEST_SUCCESS, "Successfully retrieved licences data.");
         return new LicencesResponse(licenceRepository.getLicencesByKeyword(keyword)
                 .stream()
                 .map(LicenceDto::fromLicence)
