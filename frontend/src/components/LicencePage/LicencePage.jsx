3438fc7980c9660dfbb8a7845f9a091711825cfd
gszp1 <grzybekmateusz@poczta.fm>
2025-01-26 17:12:04 +0100

    Adds displaying errors.

--- frontend/src/components/LicencePage/LicencePage.jsx
+++ frontend/src/components/LicencePage/LicencePage.jsx
@@ -2,11 +2,13 @@ import styles from "@/components/LicencePage/LicencePage.module.css"
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
+import { getReasonPhrase } from "http-status-codes";
 
 const LicencePage = () => {
     const location = useLocation();
     const {licence} = location.state;
     const [description, setDescription] = useState(null);
+    const [executionError, setExecutionError] = useState(null);
 
     useEffect(() => {
         const fetchDescription = async () => {
@@ -16,20 +18,45 @@ const LicencePage = () => {
                 let response = await axios.get(url);
                 setDescription(response.data.description)
             } catch (error) {
-                setDescription('TODO' + error);            
+                setExecutionError(error);
             }
         }
      
         fetchDescription();
     }, [licence])
 
+    const displayError = () => {
+        let errorMessage = "Server does not respond - try again later.";
+        let errorHeader = "Server error";
+        let errorStatus = "Unknown";
+        if (executionError.response) {
+            if (executionError.response.status) {
+                errorHeader = executionError.response.status;
+                errorStatus = getReasonPhrase(executionError.response.status);
+            }
+            if (executionError.response.data) {
+                errorMessage = executionError.response.data;
+            }
+            setExecutionError(null);
+        }
+        return (
+            <div className={styles.error_box}>
+                <p className={styles.error_header}>{errorHeader}</p>
+                <p className={styles.error_status}>{`Status: ${errorStatus}`}</p>
+                <p className={styles.error_message}>{errorMessage}</p>
+            </div>
+        );
+    }
+
     return (
         <div className={styles.page}>
             <div className={styles.data_box}>
             </div>
-            <div className={styles.description}>
-                {description || 'Description for this licence was not provided.'}
-            </div>
+            {executionError ? displayError() : (
+                <div className={styles.description}>
+                    {description || 'Description for this licence was not provided.'}
+                </div>
+            )}
         </div>
     )
 }
