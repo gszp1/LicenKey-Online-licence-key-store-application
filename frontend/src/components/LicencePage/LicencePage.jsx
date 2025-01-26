7ef585953e93eb118a9f7624a136e727e5324080
gszp1 <grzybekmateusz@poczta.fm>
2025-01-26 15:13:37 +0100

    Adds LicencePage component.

---
+++ frontend/src/components/LicencePage/LicencePage.jsx
@@ -0,0 +1,11 @@
+import styles from "@/components/LicencePage/LicencePage.module.css"
+
+const LicencePage = () => {
+    return (
+        <div className={styles.page}>
+
+        </div>
+    )
+}
+
+export default LicencePage;
