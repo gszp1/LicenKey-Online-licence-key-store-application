e7e4301c52863c36ee740e21a1b866a453d352fd
gszp1 <grzybekmateusz@poczta.fm>
2025-01-27 17:11:29 +0100

    Adds mapping Shopping cart contents to confirmed carts contents.

--- Backend/src/main/java/com/gszp/backend/service/OrderService.java
+++ Backend/src/main/java/com/gszp/backend/service/OrderService.java
@@ -4,27 +4,36 @@ import com.gszp.backend.auth.model.User;
 import com.gszp.backend.exception.ResourceNotFoundException;
 import com.gszp.backend.logs.LogGenerator;
 import com.gszp.backend.logs.LogTemplate;
+import com.gszp.backend.model.ConfirmedCart;
+import com.gszp.backend.model.Licence;
+import com.gszp.backend.model.ShoppingCart;
+import com.gszp.backend.model.keys.ConfirmedCartKey;
+import com.gszp.backend.repository.ConfirmedCartRepository;
 import com.gszp.backend.repository.LicenceRepository;
 import com.gszp.backend.repository.ShoppingCartRepository;
 import com.gszp.backend.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
-
-    private final ShoppingCartService shoppingCartService;
 
     private final ShoppingCartRepository shoppingCartRepository;
 
     private final UserRepository userRepository;
 
     private final LicenceRepository licenceRepository;
+    private final ConfirmedCartRepository confirmedCartRepository;
 
+    @Transactional
     public UUID createOrder(
             String userEmail
     ) throws ResourceNotFoundException {
@@ -36,6 +45,30 @@ public class OrderService {
             LogGenerator.generateInfoLog(LogTemplate.REQUEST_FAIL, "User's shopping cart is empty.");
             throw new ResourceNotFoundException("Shopping cart is empty");
         }
+        // generate UUID for order
+        final UUID orderUUID = UUID.randomUUID();
+        // change cart contents into confirmed cart contents
+        List<ConfirmedCart> confirmedCarts = shoppingCartEntries
+                .stream()
+                .map(entry -> mapToConfirmedCart(entry, orderUUID))
+                .toList();
+        confirmedCarts = confirmedCartRepository.saveAll(confirmedCarts);
+        // update licences
+        List<Licence> licences = shoppingCartEntries
+                .stream()
+                .map(ShoppingCart::getLicence)
+                .collect(Collectors.toList());
+        for (Licence licence : licences) {
+            licence.getConfirmedCartEntries().addAll(confirmedCarts);
+        }
+        licences = licenceRepository.saveAll(licences);
+        // update user
+        user.getConfirmedCartEntries().addAll(confirmedCarts);
+        user = userRepository.save(user);
+        // clear cart
+        shoppingCartRepository.deleteAll(shoppingCartEntries);
+        // send Event with UUID - TODO
+        return orderUUID;
     }
 
     private User getUserByEmail(String userEmail) throws ResourceNotFoundException {
@@ -45,5 +78,17 @@ public class OrderService {
             throw new ResourceNotFoundException("User with given email does not exist");
         }
         return userOp.get();
+    }
+
+    private ConfirmedCart mapToConfirmedCart(ShoppingCart shoppingCart, UUID orderUUID) {
+        return ConfirmedCart.builder()
+                .price(shoppingCart.getLicence().getPrice())
+                .orderIdentifier(orderUUID)
+                .quantity(shoppingCart.getQuantity())
+                .user(shoppingCart.getUser())
+                .licence(shoppingCart.getLicence())
+                .user(shoppingCart.getUser())
+                .key(new ConfirmedCartKey(shoppingCart.getUser().getUserId(), shoppingCart.getKey().getLicenceId()))
+                .build();
     }
 }
