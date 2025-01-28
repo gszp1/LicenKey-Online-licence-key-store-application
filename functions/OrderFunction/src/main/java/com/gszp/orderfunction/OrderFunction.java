b8ffc6606934093cdac641889d29dcf393c4e571
gszp1 <grzybekmateusz@poczta.fm>
2025-01-29 00:59:25 +0100

    Adds handleOrderEvent consumer.

---
+++ functions/OrderFunction/src/main/java/com/gszp/orderfunction/OrderFunction.java
@@ -0,0 +1,20 @@
+package com.gszp.orderfunction;
+
+import io.cloudevents.CloudEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
+
+@Component
+public class OrderFunction {
+
+    @Bean
+    public Consumer<CloudEvent> handleOrderEvent() {
+        return event -> {
+            
+        };
+    }
+
+}
+
