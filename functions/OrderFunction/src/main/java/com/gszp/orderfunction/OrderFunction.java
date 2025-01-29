b982efca22bb9f8de002775e460dc1b99951c90b
gszp1 <grzybekmateusz@poczta.fm>
2025-01-29 09:36:40 +0100

    Adds repository method for retrieving all cards with given UUID.

--- functions/OrderFunction/src/main/java/com/gszp/orderfunction/OrderFunction.java
+++ functions/OrderFunction/src/main/java/com/gszp/orderfunction/OrderFunction.java
@@ -25,7 +25,7 @@ public class OrderFunction {
                 return;
             }
             var eventData = objectMapper.convertValue(event, OrderEventDto.class);
-            a
+
         };
     }
 
--- functions/OrderFunction/src/main/java/com/gszp/orderfunction/repository/ConfirmedCartRepository.java
+++ functions/OrderFunction/src/main/java/com/gszp/orderfunction/repository/ConfirmedCartRepository.java
@@ -2,9 +2,16 @@ package com.gszp.orderfunction.repository;
 
 import com.gszp.orderfunction.model.ConfirmedCart;
 import com.gszp.orderfunction.model.ConfirmedCartKey;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
 public interface ConfirmedCartRepository extends JpaRepository<ConfirmedCart, ConfirmedCartKey> {
+
+    @EntityGraph(attributePaths = {"licence", "user"})
+    List<ConfirmedCart> getConfirmedCartsByOrOrderIdentifier(UUID orderIdentifier);
 }
