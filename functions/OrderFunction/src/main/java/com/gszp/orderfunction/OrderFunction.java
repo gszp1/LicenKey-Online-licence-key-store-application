2a71de82b4d34770b7f235582eb36febd8473adf
gszp1 <grzybekmateusz@poczta.fm>
2025-01-29 10:16:19 +0100

    Adds clearing shopping cart.

--- functions/OrderFunction/src/main/java/com/gszp/orderfunction/OrderFunction.java
+++ functions/OrderFunction/src/main/java/com/gszp/orderfunction/OrderFunction.java
@@ -2,17 +2,19 @@ package com.gszp.orderfunction;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gszp.orderfunction.dto.OrderEventDto;
-import com.gszp.orderfunction.model.ConfirmedCart;
-import com.gszp.orderfunction.model.Order;
-import com.gszp.orderfunction.model.OrderKey;
+import com.gszp.orderfunction.model.*;
 import com.gszp.orderfunction.repository.ConfirmedCartRepository;
+import com.gszp.orderfunction.repository.LicenceRepository;
 import com.gszp.orderfunction.repository.OrderRepository;
+import com.gszp.orderfunction.repository.UserRepository;
 import io.cloudevents.CloudEvent;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -30,7 +32,12 @@ public class OrderFunction {
 
     private final OrderRepository orderRepository;
 
+    private final LicenceRepository licenceRepository;
+
+    private final UserRepository userRepository;
+
     @Bean
+    @Transactional
     public Consumer<CloudEvent> handleOrderEvent() {
         return event -> {
             Optional<OrderEventDto> dtoOp = readEventData(event);
@@ -38,6 +45,7 @@ public class OrderFunction {
                 log.info("Event data is empty");
                 return;
             }
+            // Retrieve event data from CloudEvent
             var eventData = objectMapper.convertValue(event, OrderEventDto.class);
             List<ConfirmedCart> confirmedCartEntries =
                     confirmedCartRepository.getConfirmedCartsByOrOrderIdentifier(eventData.getOrderUUID());
@@ -45,11 +53,26 @@ public class OrderFunction {
                 log.info("No confirmed cart entries found");
                 return;
             }
+            // Map cart entries to order entries
             final UUID orderUUID = eventData.getOrderUUID();
             var orderEntries = confirmedCartEntries.stream()
                     .map(entry -> mapCartEntryToOrder(entry, orderUUID))
                     .collect(Collectors.toList());
-            
+            orderEntries = orderRepository.saveAll(orderEntries);
+            // Update user record
+            var user = orderEntries.getFirst().getUser();
+            user.getOrders().addAll(orderEntries);
+            // Update licences records
+            List<Licence> licences = new ArrayList<>();
+            for (Order order : orderEntries) {
+                Licence licence = order.getLicence();
+                licence.getOrders().add(order);
+                licences.add(licence);
+            }
+            licences = licenceRepository.saveAll(licences);
+            // Clear Confirmed Shopping Cart
+            clearConfirmedCart(confirmedCartEntries, user);
+
         };
     }
 
@@ -74,6 +97,21 @@ public class OrderFunction {
                 .quantity(confirmedCart.getQuantity())
                 .unitPrice(confirmedCart.getPrice())
                 .build();
+    }
+
+    private void clearConfirmedCart(
+            List<ConfirmedCart> confirmedCartEntries, User user
+    ) {
+        user.getConfirmedCartEntries().removeAll(confirmedCartEntries);
+        userRepository.save(user);
+        List<Licence> licences = new ArrayList<>();
+        confirmedCartEntries.forEach(confirmedCart -> {
+            Licence licence = confirmedCart.getLicence();
+            licence.getConfirmedCartEntries().remove(confirmedCart);
+            licences.add(licence);
+        });
+        licenceRepository.saveAll(licences);
+        confirmedCartRepository.deleteAll(confirmedCartEntries);
     }
 }
 
--- functions/OrderFunction/src/main/java/com/gszp/orderfunction/repository/LicenceRepository.java
+++ functions/OrderFunction/src/main/java/com/gszp/orderfunction/repository/LicenceRepository.java
@@ -1,4 +1,9 @@
 package com.gszp.orderfunction.repository;
 
-public interface LicenceRepository {
+import com.gszp.orderfunction.model.Licence;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LicenceRepository extends JpaRepository<Licence, Long> {
 }
