f85c2d72a6bf9374da10b00644ba230c67f516ea
gszp1 <grzybekmateusz@poczta.fm>
2025-02-01 00:43:37 +0100

    Adds styling to KeysSection.

--- frontend/src/components/AccountPage/Sections/Keys/KeysSection.jsx
+++ frontend/src/components/AccountPage/Sections/Keys/KeysSection.jsx
@@ -1,13 +1,16 @@
 import styles from "@/components/AccountPage/Sections/Keys/KeysSection.module.css"
+import { useState } from "react";
 
 const KeysSection = () => {
+    const [keys, setKeys] = useState([]);
+
     return (
         <div className={styles.section}>
             <h1 className={styles.section_header}> Your Licence Keys </h1>
             <div className={styles.section_content}>
                 <p className={styles.keys_header}> Keys </p>
                 <div className={styles.keys_field}>
-                    
+
                 </div>
             </div>
         </div>
--- frontend/src/components/AccountPage/Sections/Orders/OrdersSection.jsx
+++ frontend/src/components/AccountPage/Sections/Orders/OrdersSection.jsx
@@ -4,8 +4,14 @@ const OrdersSection = () => {
     return (
         <div className={styles.section}>
             <h1 className={styles.section_header}> Your Orders </h1>
+            <div className={styles.section_content}>
+                <p className={styles.orders_header}> Orders </p>
+                <div className={styles.orders_field}>
+
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default OrdersSection;
