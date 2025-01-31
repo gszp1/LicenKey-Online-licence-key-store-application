6e5320299ccd476278df68f21dc78dc6c301716f
gszp1 <grzybekmateusz@poczta.fm>
2025-01-31 23:46:54 +0100

    Adds section structure to Keys section.

--- frontend/src/components/AccountPage/Sections/Keys/KeysSection.jsx
+++ frontend/src/components/AccountPage/Sections/Keys/KeysSection.jsx
@@ -4,6 +4,12 @@ const KeysSection = () => {
     return (
         <div className={styles.section}>
             <h1 className={styles.section_header}> Your Licence Keys </h1>
+            <div className={styles.section_content}>
+                <p className={styles.keys_header}> Keys </p>
+                <div className={styles.keys_field}>
+                    
+                </div>
+            </div>
         </div>
     )
 }
