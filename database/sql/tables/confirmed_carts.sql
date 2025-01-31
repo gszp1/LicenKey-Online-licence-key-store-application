cd8e4a1f957e88b51de7fd0547bb92c2c6b7256f
gszp1 <grzybekmateusz@poczta.fm>
2025-01-31 22:19:52 +0100

    Updates primary key in confirmed_carts to include order_identifier.

--- database/sql/tables/confirmed_carts.sql
+++ database/sql/tables/confirmed_carts.sql
@@ -1,12 +1,12 @@
 CREATE TABLE confirmed_carts(
     FK_user_id BIGINT,
     FK_licence_id BIGINT,
-    quantity INT NOT NULL CHECK (quantity > 0),
     order_identifier UUID NOT NULL,
+    quantity INT NOT NULL CHECK (quantity > 0),
     price DECIMAL(10, 2) NOT NULL,
     creation_date TIMESTAMPTZ NOT NULL DEFAULT now(),
     update_date TIMESTAMPTZ NOT NULL DEFAULT now(),
-    PRIMARY KEY (FK_user_id, FK_licence_id),
+    PRIMARY KEY (FK_user_id, FK_licence_id, order_identifier),
     FOREIGN KEY (FK_user_id) REFERENCES users(user_id) ON DELETE RESTRICT,
     FOREIGN KEY (FK_licence_id) REFERENCES licences(licence_id) ON DELETE RESTRICT
 );
