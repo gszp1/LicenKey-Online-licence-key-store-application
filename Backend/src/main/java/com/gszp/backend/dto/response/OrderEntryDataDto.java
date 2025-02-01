1135a7c7add29e31c9674885ea3f965e85abb71f
gszp1 <grzybekmateusz@poczta.fm>
2025-02-01 12:11:52 +0100

    Adds mapping function from Order to OrderEntryDataDto.

--- Backend/src/main/java/com/gszp/backend/dto/response/OrderEntryDataDto.java
+++ Backend/src/main/java/com/gszp/backend/dto/response/OrderEntryDataDto.java
@@ -1,5 +1,6 @@
 package com.gszp.backend.dto.response;
 
+import com.gszp.backend.model.Order;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -16,4 +17,12 @@ public class OrderEntryDataDto {
     private BigDecimal price;
 
     private Integer quantity;
+
+    public static OrderEntryDataDto fromOrder(Order order) {
+        return OrderEntryDataDto.builder()
+                .price(order.getUnitPrice())
+                .licenceName(order.getLicence().getName())
+                .quantity(order.getQuantity())
+                .build();
+    }
 }
