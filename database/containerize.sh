3bbd6ce462026771705fd572e55c4ce14578224a
gszp1 <grzybekmateusz@poczta.fm>
2025-02-01 13:36:00 +0100

    Changes run.sh to containerize.sh in database directory.

--- database/containerize.sh
+++ database/containerize.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-docker stop licen_key_db
-sleep 1
-
 docker rm licen_key_db
 sleep 1
 
@@ -10,5 +7,3 @@ docker image rm licen_key_db
 sleep 1
 
 docker build -t licen_key_db .
-
-docker run --name licen_key_db -d -p 5432:5432 licen_key_db
