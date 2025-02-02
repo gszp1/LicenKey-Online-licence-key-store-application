525c809eabd806b4c3ebdaee013b4cabfda2b5ef
gszp1 <grzybekmateusz@poczta.fm>
2025-02-03 00:16:55 +0100

    Adds service for generating keys.

---
+++ KeyGenerationService/src/main/java/com/gszp/keygenerationservice/KeyGenerationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gszp.keygenerationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyGenerationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyGenerationServiceApplication.class, args);
+    }
+
+}
