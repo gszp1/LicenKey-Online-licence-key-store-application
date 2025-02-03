3edea1c302c9fc410f87df42b4f5ec34eb8e2a89
gszp1 <grzybekmateusz@poczta.fm>
2025-02-03 01:07:38 +0100

    Adds jar name config.

--- KeyGenerationService/build.gradle.kts
+++ KeyGenerationService/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.gszp"
-version = "0.0.1-SNAPSHOT"
+version = "1.0.0"
 
 java {
     toolchain {
@@ -31,4 +31,9 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    archiveFileName.set("key-generation-service.jar")
+    mainClass.set("com.gszp.keygenerationservice.KeyGenerationServiceApplication")
 }
