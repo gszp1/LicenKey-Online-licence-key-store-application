a57e5e99c3b5206f01925685bc34286f1cb743d6
gszp1 <grzybekmateusz@poczta.fm>
2025-02-03 01:22:24 +0100

    Adds cors configuration.

---
+++ KeyGenerationService/src/main/java/com/gszp/keygenerationservice/config/CorsConfiguration.java
@@ -0,0 +1,27 @@
+package com.gszp.keygenerationservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowCredentials(false)
+                        .maxAge(3600);
+            }
+        };
+    }
+
+}
